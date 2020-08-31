d4d51d2405217edbcae1e24b364ff8b8d2898eb6
Erick PC <erksters@gmail.com>
2020-08-30 21:00:55 -0500

    rendering home page with cards

--- lofitees/src/api/api.js
+++ lofitees/src/api/api.js
@@ -1,7 +1,7 @@
 // const myServer = "https://myboopostgres.herokuapp.com/";
-const myServer = "http://127.0.0.1:8000/";
-export const allShirts = myServer + "all_shirts";
-export const createOrder = myServer + "create_order";
+export const myServer = "http://127.0.0.1:8000/";
+export const allShirts = myServer + "api/allshirts";
+export const createOrder = myServer + "api/createorder";
 export const individualShirt = myServer + "individual_shirt";
 export const loginServer = myServer + "api/login";
-export const findToken = localStorage.getItem("lofiteestoken") || false;
+export const findToken = sessionStorage.getItem("lofiteestoken") || false;
--- lofitees/src/components/Home/Home.js
+++ lofitees/src/components/Home/Home.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Loading from "../Loading/Loading";
-import { allShirts } from "../../api/api";
-import { Button } from "react-bootstrap";
+import { allShirts, myServer } from "../../api/api";
+import { Button, Card } from "react-bootstrap";
 import { findToken } from "../../api/api";
 
 const Home = (props) => {
   const [shirts, setShirts] = useState([]);
 
   console.log(findToken);
-
   const loadData = async () => {
     await fetch(allShirts)
       .then((response) => {
@@ -26,18 +25,48 @@ const Home = (props) => {
 
   console.log("all shirts", shirts);
 
-  // if (!shirts.length) {
-  //   return <Loading />;
-  // }
+  if (!shirts.length) {
+    return <Loading />;
+  }
   return (
     <>
+      {/* Desktop */}
       <div className="d-none d-lg-block">
         <div style={{ textAlign: "center", fontSize: 28, fontWeight: "bold" }}>
           Check Out Our <br />
           Latest Designs
         </div>
+        <div>
+          {shirts.map((shirt) => (
+            <>
+              <Card
+                style={{
+                  width: 500,
+                  display: "inline-block",
+                  textAlign: "center",
+                }}
+              >
+                <Card.Header>{shirt.title}</Card.Header>
+                <Card.Body>
+                  <img
+                    src={`${myServer}media/${shirt.img}`}
+                    alt="whoops"
+                    width={400}
+                    height={400}
+                  />
+                  <br />
+                  <div style={{ textAlign: "center" }}>{shirt.description}</div>
+                </Card.Body>
+                <Card.Footer>
+                  <Button href={`${shirt.title}`}>See More Details</Button>
+                </Card.Footer>
+              </Card>
+            </>
+          ))}
+        </div>
       </div>
 
+      {/* Mobile */}
       <div className="d-lg-none">
         <div style={{ textAlign: "center", fontSize: 28, fontWeight: "bold" }}>
           Check Out Our <br />
