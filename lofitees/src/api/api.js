ccb782e028808f32465ae677743f6f94af2429b4
Erick PC <erksters@gmail.com>
2020-09-05 17:27:40 -0500

    added create_order api

--- lofitees/src/api/api.js
+++ lofitees/src/api/api.js
@@ -1,8 +1,11 @@
-// const myServer = "https://myboopostgres.herokuapp.com/";
+// const myServer = "https://lofiteespostgres.herokuapp.com/";
 export const myServer = "http://127.0.0.1:8000/";
 export const allShirts = myServer + "api/allshirts";
-// export const createOrder = myServer + "api/createorder";
+export const createOrder = myServer + "api/create_order";
 export const singleShirt = myServer + "api/get_shirt/";
 export const loginServer = myServer + "api/login";
 export const logoutServer = myServer + "api/logout";
 export const findToken = sessionStorage.getItem("lofiteestoken") || false;
+export const findToken = sessionStorage.getItem("lofiteesusername") || false;
+export const findToken =
+  sessionStorage.getItem("lofiteeslocationprofile") || false;
